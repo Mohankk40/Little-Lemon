11f25b9fa129de14b35e57624731e0ca99e6cc45
Mohana Krishnan <mohankk40@gmail.com>
2024-11-02 02:08:31 +0530

    Updated site with header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+
 
 function App() {
   return (
+    <>
+      <Layout>
+
+      </Layout>
+    </>
   );
 }
 
