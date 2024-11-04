4ba69da233d2dab64b9df215ce638cdbb6d5da1b
Mohana Krishnan <mohankk40@gmail.com>
2024-11-04 12:41:43 +0530

    Updated the booking page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import pages from './utils/pages';
 import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings';
 import NotFound from './components/pages/NotFound';
 import UnderConstruction from './components/pages/UnderConstruction'
 
@@ -17,6 +19,11 @@ function App() {
 
           <Route path={pages.get('menu').path} 
           element={<UnderConstruction />} />
+
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+
+          <Route path={pages.get('confirmedBooking').path}
+          element={<ConfirmedBooking />} />
 
           <Route path={pages.get('orderOnline').path}
           element={<UnderConstruction />} />
